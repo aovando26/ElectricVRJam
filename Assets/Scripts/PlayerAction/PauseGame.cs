087eff564ec1c47a99de888349e696b13a0aed41
aovando <aovandodevsxr@gmail.com>
2024-06-27 15:27:48 -0400

    pause game script added

---
+++ Assets/Scripts/PlayerAction/PauseGame.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    void Start()
+    {
+
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true); // show the pause UI
+        Time.timeScale = 0f; // stops the game time    
+    }
+
+    public void Continue()
+    {
+        pausePanel.SetActive(false); // hides the pause UI
+        Time.timeScale = 1f; // resume the game time
+    }
+}
